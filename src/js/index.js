aa515cf54679d5228ddfe30cb795e4dcfb670728
Arthur Gama <arthur.m.gama99@gmail.com>
2025-02-26 21:30:37 -0300

    adicionados arquivos da quest

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+document.getElementById("btn-enviar").addEventListener('click', () => {
+    const nome = document.getElementById('nome');
+    const email = document.getElementById('email');
+    const telefone = document.getElementById('telefone');
+    const mensagem = document.getElementById('mensagem');
+    
+    const obrigatorioNome = document.getElementById('nome-obrigatorio');
+    const obrigatorioEmail = document.getElementById('email-obrigatorio');
+    const obrigatorioTelefone = document.getElementById('telefone-obrigatorio');
+    const obrigatorioMensagem = document.getElementById('mensagem-obrigatorio');
+
+    function validarFormulario(campo, mensagemDeErro) {
+        if (campo.value.trim() === '') {
+            campo.classList.add('borda-vermelha');
+            campo.classList.remove('borda-verde');
+            mensagemDeErro.style.display = 'block';
+        } else {
+            campo.classList.remove('borda-vermelha');
+            campo.classList.add('borda-verde');
+            mensagemDeErro.style.display = 'none';
+        }
+    }
+    
+    const validoNome = validarFormulario(nome, obrigatorioNome);
+    const validoEmail = validarFormulario(email, obrigatorioEmail);
+    const validoTelefone = validarFormulario(telefone, obrigatorioTelefone);
+    const validoMensagem = validarFormulario(mensagem, obrigatorioMensagem);
+});
